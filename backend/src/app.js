3bd91f22c3c9282872bf8cbed47727c8e2bf269c
caue peruque <caue19052003@gmail.com>
2023-08-14 14:54:07 -0300

    feat: created connection mysql

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { productsModel } = require('./models');
 
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.json({ status: 'Store Manager UP!' });
+});
+
+app.get('/products', async (_req, res) => {
+  const products = await productsModel.findAll();
+  res.status(200).json(products);
 });
 
 module.exports = app;
---
+++ backend/src/models/products.model.js
@@ -0,0 +1,12 @@
+const connection = require('./connection');
+
+const findAll = async () => {
+  const [products] = await connection.execute(
+    'SELECT * FROM products',
+  );
+  return products;
+};
+
+module.exports = {
+  findAll,
+};
